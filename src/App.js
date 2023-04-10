9025b0e34deaae0202f04c40416da7a5102e63b3
mahdimalkawi1 <mahdimalkawi66@gmail.com>
2023-04-10 23:15:23 +0000

    lab19-1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import Home from './components/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FavList from './components/FavList/FavList';
+import { Navbarr } from './components/Navbar/Navbar';
+import {   Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
+    // <>
+    //   {/* <Home/> */}
+    // </>
     <>
-      <Home/>
-    </>
+    <Navbarr />
+    <Routes>
+      <Route path='/' element={<Home />} ></Route>
+      <Route path='/FavList' element={<FavList />} ></Route>
+    </Routes>
+
+
+  </>
   );
 }
 
--- src/components/FavList/FavList.js
+++ src/components/FavList/FavList.js
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 
 function FavList(){
 
@@ -12,6 +15,34 @@ async function getFavMovie(){
     let recivedData = await response.json();
     setFavMovies(recivedData)
 }
+
+async function handleDelete(id){
+    let url = `${process.env.REACT_APP_SERVER_URL}/deleteMovie/${id}`;
+    let response = await fetch(url,{
+        method: "DELETE",
+    headers: {
+        "content-Type": "application/json"
+    },
+    })
+if (response.status ===204){
+    getFavMovie()
+
+    // alert("successfully deleted")
+}
+
+}
+async function handleUpdate(id,data){
+    let url = `${process.env.REACT_APP_SERVER_URL}/updateMovie/${id}`;
+    let response = await fetch(url,{
+        method: "PUT",
+    headers: {
+        "content-Type": "application/json"
+    },
+    body: JSON.stringify(data),
+
+    })
+}
+
 useEffect(()=>{
     getFavMovie()
 
@@ -23,14 +54,16 @@ return(
     <>
     <h2>this is Fav Movie Page</h2>
     {
-        favMovies && favMovies.map(movie=>{
+        favMovies && favMovies.map (props.movie => {
              return (
     
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} />
         <Card.Body>
-          <Card.Title>{movie.title}</Card.Title>
-          <Button variant="primary" >hello</Button>
+          <Card.Title>{props.movie.title}</Card.Title>
+          <Button variant="primary" onClick={()=>handleDelete(props.movie.id)} >Delete</Button>
+          <Button variant="primary" onClick={()=>handleUpdate(props.movie.id)} >Update</Button>
+
         </Card.Body>
       </Card>
 
